bf86d60cf9c6dd666c0ad7c32f532f2f6c41b57c
GuidoRM <guidorafaelmamani100@gmail.com>
2023-02-11 17:32:38 -0300

    Creación del servidor con express

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const path = require ("path");
+const port = process.env.PORT || 3030;
+
+app.listen(port,()=>{
+    console.log("Running on: "+port);
+})
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get("/",(req,res)=>{
+     res.sendFile(path.join(__dirname,"/app/views/main.html"));
+})
