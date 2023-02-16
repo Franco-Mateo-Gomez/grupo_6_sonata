2af202b9cc8d23b7abb755ae1c3a576466af1246
GuidoRM <guidorafaelmamani100@gmail.com>
2023-02-16 15:14:23 -0300

    prueba

--- public/scripts/script-slider-responsive.js
+++ public/scripts/script-slider-responsive.js
@@ -1,23 +1,23 @@
 var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
-  initialSlide:3, /**Inicializo en la diapositiva 4 */
+  initialSlide: 3, /**Inicializo en la diapositiva 4 */
   grabCursor: true, /**Genera al pasar sobre la diapositiva la manito del cursor */
   centeredSlides: true,
   slidesPerView: "auto",
-  spaceBetween:20, /**Espacio entre diapositivas o cartas**/
+  spaceBetween: 20, /**Espacio entre diapositivas o cartas**/
   coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 200,
-      modifier: 1.5,
-      slideShadows: true,
+    rotate: 0,
+    stretch: 0,
+    depth: 200,
+    modifier: 1.5,
+    slideShadows: true,
   },
   pagination: {
-      el: ".swiper-pagination",
+    el: ".swiper-pagination",
   },
-  autoplay:{
+  autoplay: {
     delay: 3000 /*LOOP DE 1 segundo**/
   },
-  loop:true
+  loop: true
 });
 swiper.init(true);
