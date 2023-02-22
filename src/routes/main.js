605f92ed3a1ce72ef80f0479c55301e52fc89fdd
Abril <abrilmartinezcata@gmail.com>
2023-02-21 22:52:58 -0300

    pruebas

--- src/routes/main.js
+++ src/routes/main.js
@@ -18,4 +18,8 @@ router.get("/login",(req,res)=>{
     res.sendFile(path.join(__dirname,"../views/login.html"));
 })
 
+router.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../views/register.html"));
+})
+
 module.exports=router;
