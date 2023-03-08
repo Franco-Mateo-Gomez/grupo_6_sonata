cfed6ca0de083876f7053d131e419b2d9ad2c0c3
Franco-Mateo-Gomez <matufranone@gmail.com>
2023-03-08 13:49:57 -0300

    Plantilla de géneros dinámica + ajustes css

--- src/controllers/generesControllers.js
+++ src/controllers/generesControllers.js
@@ -1,7 +1,22 @@
 
+const generesRock=[
+    {
+        name:"Skrillex",
+        image:"/images/imagen_album3.JPG",
+    },
+    {
+        name:"Mona Lisa",
+        image:"/images/imagen_album2.JPG",
+    },
+    {
+        name:"Carl",
+        image:"/images/imagen_album4.JPG",
+    }
+]
+
 const generesController={
     rock:(req,res) =>{
-        res.render("generesRock");
+        res.render("generesRock",{generesRock:generesRock});
     }
 }
 
