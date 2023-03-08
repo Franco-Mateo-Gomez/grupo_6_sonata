2c0e141aa4d1af3aa1cccb69a65fff9540418074
Franco-Mateo-Gomez <matufranone@gmail.com>
2023-03-08 00:43:34 -0300

    Página de listado de productos + Responsive

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -3,8 +3,14 @@ const productsController={
     productDetail:(req,res) =>{
         res.render("products/productDetail");
     },
+    productCreate:(req,res)=>{
+        res.render("products/createProduct")
+    },
     productEdit:(req,res)=>{
-        res.render("editProduct")
+        res.render("products/editProduct")
+    },
+    productEditList:(req,res)=>{
+        res.render("products/editProductList")
     },
     adminProducts:(req,res) =>{
         res.render("products/adminProducts");
--- src/routes/products.js
+++ src/routes/products.js
@@ -5,4 +5,7 @@ const productsController = require("../controllers/productsControllers.js")
 /*Principal page*/
 router.get("/detail",productsController.productDetail);
 router.get("/admin-products",productsController.adminProducts);
+router.get("/admin-products/create",productsController.productCreate);
+router.get("/admin-products/edit",productsController.productEdit);
+router.get("/admin-products/edit-list",productsController.productEditList);
 module.exports = router;
