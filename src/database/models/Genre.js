edbdeb69068f4d0ea8a723537ec65f487c1baa4f
GuidoRM <guidorafaelmamani100@gmail.com>
2023-04-17 00:05:08 -0300

    Creación de Modelo de tabla para los Generos + Relaciones

---
+++ src/database/models/Genre.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Genres";
+
+    let colums = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false, //NOT NULL
+            autoIncrement: true
+        },
+        name: {
+            type: dataTypes.STRING(255),
+            allowNull: false //NOT NULL
+        }
+    };
+
+    let config = {
+        tableName: "genres",
+        timestamps: false
+    };
+
+    const Genre = sequelize.define(alias, colums, config);
+
+    //Relacionamos un Genero con muchos Albumos
+    Genre.associate = function (models) {
+        Genre.hasMany(models.Albums, {
+            as: "albums", //Nombre de la relación
+            foreignKey: "idGenre_Fk"
+        })
+    }
+
+    return Genre;
+
+}
