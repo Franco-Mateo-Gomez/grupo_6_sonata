1e19a785a1dfc2e84d20f27a9aca270bb28057d0
GuidoRM <guidorafaelmamani100@gmail.com>
2023-04-17 00:03:48 -0300

    Creación de Modelo de tabla para los albumes + Relaciones

---
+++ src/database/models/Album.js
@@ -0,0 +1,53 @@
+module.exports = (sequelize, dataTypes)=>{
+
+    let alias = "Albums";
+
+    let colums = {
+        id:{
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false, //NOT NULL
+            autoIncrement : true
+        },
+        name:{
+            type: dataTypes.STRING(50),
+            allowNull: false //NOT NULL
+        },
+        description:{
+            type: dataTypes.TEXT,
+            allowNull: false, //NOT NULL
+            unique: true
+        },
+        image:{
+            type: dataTypes.STRING(255),
+            allowNull: false, //NOT NULL
+            defaultValue:'/images/users/default.jpg'
+        },
+        rate:{
+            type: dataTypes.TINYINT,
+            allowNull: false, //NOT NULL
+            unique: true
+        },
+        dateUpload:{
+            type: dataTypes.DATE,
+            allowNull: false, //NOT NULL
+        }
+    };
+
+    let config = {
+        tableName: "albums",
+        timestamps: false
+    };
+
+    const Album = sequelize.define(alias,colums,config);
+
+    //Relacionamos Albumes con un Genero
+    Album.associate = function(models){
+        Album.belongsTo(models.Genres,{
+            as: "genres", //Nombre de la relación
+            foreignKey: "idGenre_Fk"
+        })
+    }
+
+    return Album;
+}
