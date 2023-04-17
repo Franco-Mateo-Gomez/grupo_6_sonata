6bed06ffe89cfb29e02d3e0de73c1f771f5c566c
GuidoRM <guidorafaelmamani100@gmail.com>
2023-04-16 22:30:42 -0300

    Creación de Modelo de tabla para los usuarios

---
+++ src/database/models/User.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, dataTypes)=>{
+    let alias = "Users";
+    let colums = {
+        id:{
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false, //NOT NULL
+            autoIncrement : true
+        },
+        fullName:{
+            type: dataTypes.STRING(50),
+            allowNull: false //NOT NULL
+        },
+        userName:{
+            type: dataTypes.STRING(50),
+            allowNull: false, //NOT NULL
+            unique: true
+        },
+        email:{
+            type: dataTypes.STRING(50),
+            allowNull: false, //NOT NULL
+            unique: true
+        },
+        password:{
+            type: dataTypes.STRING(255),
+            allowNull: false, //NOT NULL
+        },
+        image:{
+            type: dataTypes.STRING(255),
+            allowNull: false, //NOT NULL
+            defaultValue:'/images/users/default.jpg'
+        }
+    }
+    let config = {
+        tableName: "users",
+        timestamps: false
+    }
+    const User = sequelize.define(alias, colums, config);
+
+    return User;
+}
