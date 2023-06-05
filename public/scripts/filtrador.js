b400778f41f86e2cd4f13dd512f1c6a761363f77
GuidoRM <guidorafaelmamani100@gmail.com>
2023-06-04 23:57:26 -0300

    Sección de generos pagina principal

--- public/scripts/filtrador.js
+++ public/scripts/filtrador.js
@@ -1,18 +1,16 @@
-
 let contenedorAlbumes = document.querySelector(".contenedor_albumes_filtrar");
 let albumes = document.querySelector(".contenedor_albumes_filtrar").children;
-
 let botones = document.querySelector(".filtrador_botones").children;
-
 let botonNada = document.querySelector("#nada");
+let botonNada_desplegable = document.querySelector("#nada_desplegable");
+let botones_desplegable = document.querySelector(".filtrador_botones_desplegable").children;
 
-for (let i = 0; i < botones.length - 1; i++) {
 
-
+for (let i = 0; i < botones.length; i++) {
     botones[i].addEventListener("click", () => {
         let eleccion = botones[i].id + "";
-        console.log(botonNada.className.includes("seleccionada"))
-        if(botonNada.className.includes("seleccionada")){
+
+        if (botonNada.className.includes("seleccionada") || botonNada_desplegable.className.includes("seleccionada")) {
             contenedorAlbumes.classList.remove("invisible-filtro");
             botonNada.classList.remove("seleccionada");
         }
@@ -27,22 +25,85 @@ for (let i = 0; i < botones.length - 1; i++) {
                 botones[i].classList.remove("seleccionada");
             }
         }
+
+        // Remover la clase "seleccionada" de todos los botones
+        for (let j = 0; j < botones.length; j++) {
+            botones[j].classList.remove("seleccionada");
+        }
+        // Agregar la clase "seleccionada" solo al botón presionado
+        botones[i].classList.add("seleccionada");
+
     })
 }
-
 botonNada.addEventListener("click", () => {
     contenedorAlbumes.classList.toggle("invisible-filtro");
     botonNada.classList.toggle("seleccionada");
-    
+    // Remover la clase "seleccionada" de todos los botones
+    for (let j = 0; j < botones.length; j++) {
+        botones[j].classList.remove("seleccionada");
+    }
 });
 
 
+
+for (let i = 0; i < botones.length - 1; i++) {
+
+    botones_desplegable[i].addEventListener("click", () => {
+        let eleccion = botones_desplegable[i].id + "";
+
+        if (botonNada.className.includes("seleccionada") || botonNada_desplegable.className.includes("seleccionada")) {
+            contenedorAlbumes.classList.remove("invisible-filtro");
+            botonNada_desplegable.classList.remove("seleccionada");
+        }
+
+        for (let j = 0; j < albumes.length; j++) {
+            if (!albumes[j].className.includes(eleccion) && botones_desplegable[i].id.includes("seleccionada") == false) {
+                albumes[j].classList.add("invisible-filtro");
+                botones_desplegable[i].classList.add("seleccionada");
+            }
+            else {
+                albumes[j].classList.remove("invisible-filtro");
+                botones_desplegable[i].classList.remove("seleccionada");
+            }
+        }
+
+        // Remover la clase "seleccionada" de todos los botones
+        for (let j = 0; j < botones.length; j++) {
+            botones[j].classList.remove("seleccionada");
+        }
+        // Agregar la clase "seleccionada" solo al botón presionado
+        botones[i].classList.add("seleccionada");
+    })
+}
+
+botonNada_desplegable.addEventListener("click", () => {
+    contenedorAlbumes.classList.toggle("invisible-filtro");
+    botonNada_desplegable.classList.toggle("seleccionada");
+});
+
+
+let boton_filtrador_generos = document.querySelector(".boton_generos");
+let contenedor_lista_generos = document.querySelector(".filtrador_botones_desplegable");
+let boton_ocultarTodo = document.querySelector("#nada_desplegable");
+
+boton_filtrador_generos.addEventListener("click", () => {
+    contenedor_lista_generos.classList.toggle("visibleMenuDesplegableGeneros")
+});
+
+boton_ocultarTodo.addEventListener("click", () => {
+    contenedor_lista_generos.classList.toggle("visibleMenuDesplegableGeneros")
+});
+
+
+
+
+//EFECTO SOBRE LAS TARJETAS DE LOS ALBUMES
 let imagen_tarjeta = document.querySelectorAll(".imagen_tarjeta_album");
-let fondo_imagen = document.querySelectorAll(".fondo_imagen") 
+let fondo_imagen = document.querySelectorAll(".fondo_imagen")
 
 let contenedores = document.querySelectorAll(".tarjeta_album");
 
-for (let i = 0; i < contenedores.length ; i++) {
+for (let i = 0; i < contenedores.length; i++) {
 
     contenedores[i].addEventListener("mouseover", () => {
         fondo_imagen[i].classList.add("fondo_imagen_tamaño");
@@ -54,59 +115,19 @@ for (let i = 0; i < contenedores.length ; i++) {
 }
 
 
-// Obtener elementos del encabezado y la caja
-var header = document.querySelector("header");
-var caja = document.querySelector(".presentacion");
 
-// Escuchar el evento scroll
-window.addEventListener("scroll", function() {
-    header.style.transition = "background-color 1s ease-in-out";
-    // Obtener posición de la caja
-    var cajaPosicion = caja.getBoundingClientRect();
-  
-    // Obtener posición del encabezado
-    var headerPosicion = header.getBoundingClientRect();
-  
-    // Verificar si el encabezado está sobre la caja específica
-    if (headerPosicion.bottom >= cajaPosicion.top && headerPosicion.top <= cajaPosicion.bottom) {
-      header.classList.add("fondo-transparente-header");/* Cambiar el color del encabezado */
-    } else {
-    header.classList.remove("fondo-transparente-header"); /* Volver al color inicial del encabezado */
-    }
-  });
+//DESPLAZAMIENTO DE LAS CAJAS DE GENEROS
+let botonPrev = document.querySelector(".prev");
+let botonNext = document.querySelector(".next");
+let contenedorFiltros = document.querySelector(".contenedor_genero");
+let desplazamiento = 0; // Inicialmente no hay desplazamiento
 
-  // Función para verificar el estado del encabezado al cargar la página
-function verificarEstadoInicial() {
-    var cajaPosicion = caja.getBoundingClientRect();
-    var headerPosicion = header.getBoundingClientRect();
-  
-    // Verificar si el encabezado está sobre la caja específica al cargar la página
-    if (headerPosicion.bottom >= cajaPosicion.top && headerPosicion.top <= cajaPosicion.bottom) {
-        header.classList.add("fondo-transparente-header"); /* Cambiar el color del encabezado */
-    } else {
-        header.classList.remove("fondo-transparente-header"); /* Volver al color inicial del encabezado */
-    }
-  }
-  
-  // Escuchar el evento scroll
-  window.addEventListener("scroll", function() {
-    var cajaPosicion = caja.getBoundingClientRect();
-    var headerPosicion = header.getBoundingClientRect();
-  
-    // Verificar si el encabezado está sobre la caja específica al hacer scroll
-    if (headerPosicion.bottom >= cajaPosicion.top && headerPosicion.top <= cajaPosicion.bottom) {
-        header.classList.add("fondo-transparente-header"); /* Cambiar el color del encabezado */
-    } else {
-        header.classList.remove("fondo-transparente-header"); /* Volver al color inicial del encabezado */
-    }
-  });
-  
-  // Verificar el estado inicial del encabezado al cargar la página
-  verificarEstadoInicial();
+botonPrev.addEventListener("click", () => {
+    desplazamiento += 240; // Suma 100vw al desplazamiento acumulativo
+    contenedorFiltros.style.transform = `translateX(${desplazamiento}px)`;
+});
 
-  let boton_filtrador_generos = document.querySelector(".boton_generos");
-  let contenedor_lista_generos = document.querySelector(".filtrador_botones");
-
-  boton_filtrador_generos.addEventListener("click", ()=>{
-    contenedor_lista_generos.classList.toggle("visibleMenuDesplegableGeneros")
-  });
+botonNext.addEventListener("click", () => {
+    desplazamiento -= 240; // Resta 100vw al desplazamiento acumulativo
+    contenedorFiltros.style.transform = `translateX(${desplazamiento}px)`;
+});
