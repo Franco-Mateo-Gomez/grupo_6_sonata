c77bc1a59b1be22d96f254b1f0a51aa36b48e9c5
GuidoRM <guidorafaelmamani100@gmail.com>
2023-06-10 20:51:35 -0300

    Diseño para la pagina de detalles del producto

--- public/scripts/script-slider-responsive.js
+++ public/scripts/script-slider-responsive.js
@@ -1,23 +1,16 @@
 var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  initialSlide: 3, /**Inicializo en la diapositiva 4 */
-  grabCursor: true, /**Genera al pasar sobre la diapositiva la manito del cursor */
+  grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
-  spaceBetween: 20, /**Espacio entre diapositivas o cartas**/
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 200,
-    modifier: 1.5,
-    slideShadows: true,
+  spaceBetween: 20,
+  loop: true, 
+  autoplay: {
+    delay: 3000
   },
   pagination: {
     el: ".swiper-pagination",
-  },
-  autoplay: {
-    delay: 3000 /*LOOP DE 1 segundo**/
-  },
-  loop: true
+  }
+
 });
-swiper.init(true);
+
+swiper.init();
