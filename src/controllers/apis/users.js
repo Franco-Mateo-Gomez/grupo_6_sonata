10023125ca58e81f660dd30268f439262e7b4b58
mica-prog <mikaamarfil27@gmail.com>
2023-06-25 18:57:15 -0300

    Pagina de consultas

--- src/controllers/apis/users.js
+++ src/controllers/apis/users.js
@@ -3,7 +3,7 @@ let db = require ("../../database/models")
 
 const apiUsersController = {
     loadUsers: async (req,res) => {
-         
+
         let Users = await db.Users.findAll()
         let Contador = Users.length;
         let infoUsers = Users.map(user =>{
@@ -12,19 +12,19 @@ const apiUsersController = {
                 name: user.userName,
                 email: user.email,
                 image: user.image,
+                isComposer: user.isComposer,
                 detail: "/api/users/" + user.id,
             }
-            
+
             return data;
-             
+
         })
         let addDetailInUser = Users.map(user => {
             const userData = user.toJSON(); // Convert to JSON version
             userData.detail = "/api/users/" + user.id; // Add new property [detail]
             return userData;
         }) 
-        
-        
+
         let data = {
             count: Contador,
             Users: infoUsers,
@@ -34,13 +34,26 @@ const apiUsersController = {
     loadUser: async (req, res) => {
         let idUser = req.params.id 
         let User = await db.Users.findByPk (idUser)
-        let data = {
-            id: User.id,
-            name: User.userName,
-            email: User.email,
-            image: User.image,
-            description: User.description,
-            isComposer: User.isComposer,
+        let data;
+        if(User){
+            data = {
+                id: User.id,
+                name: User.userName,
+                email: User.email,
+                image: User.image,
+                description: User.description,
+                isComposer: User.isComposer,
+            }
+        }
+        else{
+            data = {
+                id: "0",
+                name: "No Existe",
+                email: "No Existe",
+                image: "No Tiene",
+                description: "No Tiene",
+                isComposer: null,
+            }
         }
         res.json(data)
     }
