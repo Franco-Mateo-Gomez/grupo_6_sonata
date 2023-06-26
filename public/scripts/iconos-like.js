579c3ae9cf0473f5cc7bb9233a6e1456160c499c
mica-prog <mikaamarfil27@gmail.com>
2023-06-26 00:36:43 -0300

    iconos de like

---
+++ public/scripts/iconos-like.js
@@ -0,0 +1,14 @@
+let icono1 = document.querySelector(".iconos .icono-uno")
+let icono2 = document.querySelector(".iconos .icono-dos")
+
+
+icono1.addEventListener("click", (e) => {
+        icono1.style.color = "green";
+    })
+    
+    
+icono2.addEventListener("click", (e) => {
+    icono2.style.color = "red";
+})
+    
+
